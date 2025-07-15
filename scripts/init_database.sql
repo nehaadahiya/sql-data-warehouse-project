e4e6f4f1836c7d144b449f6b2bdbbd223e804bc5
Neha Dahiya <104566950+nehaadahiya@users.noreply.github.com>
2025-07-15 17:17:13 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -15,7 +15,7 @@ USE master;
 GO
 
 -- DROP AND RECREATE THE 'DataWarehouse' Database
-IF EXISTS (SE;ECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
   ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
   DROP DATABASE DataWarehouse;
